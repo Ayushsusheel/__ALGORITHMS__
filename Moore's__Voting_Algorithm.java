e16d8cf0a0957a0074d29ccdb7f3676c9ceb986e
Ayush_S <60178558+Ayushsusheel@users.noreply.github.com>
2021-07-01 05:51:17 +0530

    Update Moore's__Voting_Algorithm.java
    question->majority element in array
    how to solve->by using moore's voting algorithm...

--- Moore's__Voting_Algorithm.java
+++ Moore's__Voting_Algorithm.java
@@ -1,3 +1,51 @@
 //* given an array we have to find the element whose occurence is more than size/2 times
-//multiple ways we can choose but best time & spae complexity for this algorithm is O(n)-->time ,O(1)-->for space!!!!....
+//multiple ways we can choose but best time & space complexity for this algorithm is O(n)-->time ,O(1)-->for space!!!!....
 
+public class ARRAYS
+{
+public static void main(String...strings)
+{
+Scanner sc=new Scanner(System.in);
+int size=sc.nextInt();
+int a[]=new int[size];
+for(int i=0;i<a.length;i++)
+{
+a[i]=sc.nextInt();
+}
+
+int index=0;
+int count=1;
+for(int j=1;j<a.length;j++)
+{
+if(a[j]==a[index])
+{
+count++;
+}
+else
+{
+count--;
+}
+
+if(count==0)
+{
+index=j;
+count=1;
+}
+
+}
+//int jisko_check_krna_h=a[index];
+int COUNT=0;
+for(int i=0;i<a.length;i++)
+{
+if(a[i]==a[index])
+{
+COUNT++;
+}
+if(COUNT>size/2)
+{
+System.out.println(a[i]);
+return;
+}
+}
+System.out.println("no element greater than size/2 times");
+}}
